d7b82bee290a3991030a6d94b1cbb7dca57d610c
Mohammad Alwazzan <kingwazzan2@gmail.com>
2022-08-23 12:48:52 +0300

    the edited files

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
-const container = document.querySelector(".container");
+const btn = document.createElement('button');
+btn.classList.add('btn');
+btn.textContent="adjust size";
+document.body.appendChild(btn);
 
-for(let i=0; i < 16;i++){
+function page(blocks){
+const container = document.createElement('div');
+container.classList.add('container');
+document.body.appendChild(container);
+for(let i=0; i < blocks;i++){
     const di = document.createElement('div');
     di.classList.add('child1');
     
-    for(let y=0; y<16; y++){
+    for(let y=0; y<blocks; y++){
     const d = document.createElement('div');
         d.classList.add('child2');
-        d.style.display="flex";
-        d.style.height="100px";
-        d.style.width="100px";
+        d.style.border="1px solid black";
+        d.style.flex = "1 1 auto";
         d.onmouseover = ()=>{
        d.style.backgroundColor="blue";};
     di.appendChild(d);
     container.appendChild(di);
-    
+           
     }
     
  
 }
 
+}
+page(64);
+btn.addEventListener('click',()=>{
+    let block = parseInt(prompt('Enter The new size max is 64'));
+    if(block <= 64 && block > 0){
+    const del = document.querySelector('.container');
+    del.remove();
+    page(block);
+}
+else{
+    alert("Error");
+}
+});
